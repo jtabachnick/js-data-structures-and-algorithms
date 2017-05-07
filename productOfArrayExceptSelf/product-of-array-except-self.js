39b304e7db4a3d57d2b7af30980a80c55356117d
Jeff Tabachnick <jtabachnick@gmail.com>
2017-05-06 22:38:37 -0400

    Implementing product array except self with validation.

--- productOfArrayExceptSelf/product-of-array-except-self.js
+++ productOfArrayExceptSelf/product-of-array-except-self.js
@@ -12,5 +12,49 @@
   space complexity analysis.)
 */
 
+var Product = {
+  
+  Validate: function() {
+    var input = $('#input').val();
+    return input.match(/^[0-9]+(,[0-9]+)*$/);
+  },
 
+  ReadInput: function() {
+    if (Product.Validate() != null) {
+      var input = $('#input').val().split(',');
+      return input.map(Number);
+    }
+    $('#result').html('Please enter only integers and commas');
+    throw exception;
+  },
+
+  GetProductArray: function() {
+    var input = Product.ReadInput();
+    var result = [];
+    var temp = 1;
+    for (var i = 0; i < input.length; i++) {
+      result.push(temp);
+      temp *= input[i];
+    }
+
+    temp = 1;
+    for (var i = input.length - 1; i >= 0; i--) {
+      result[i] *= temp;
+      temp *= input[i];
+    }
+
+    return result;
+  },
+
+  PrintResult: function() {
+    $('#result').html(JSON.stringify(Product.GetProductArray()));
+  }
+
+};
+
+$(document).ready(function() {
+  $('#submit').on('click', function() {
+    Product.PrintResult();
+  });
+});
 
